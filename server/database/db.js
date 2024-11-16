78e174bbe94367f5ac124f005013bf1a363a107d
Shivansh Nagar <114014805+shiv-abhi77@users.noreply.github.com>
2024-11-16 14:52:34 +0530

    second

--- server/database/db.js
+++ server/database/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import dotenv from 'dotenv'
 dotenv.config()
 const dbConnection = async (username, password) =>{
-    const URL = `mongodb+srv://shivanshnagar7555:<${process.env.DB_PASSWORD}>@cluster0.xc9regk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+    const URL = `mongodb+srv://shivanshnagar7555:${process.env.DB_PASSWORD}@cluster0.xc9regk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 
     try {
         await mongoose.connect(URL);
